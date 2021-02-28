861777d3b57a4009561dea97f892b4352e4b76f9
Quinnsicle <qtoconnor@gmail.com>
2021-02-27 20:43:22 -0600

    added bubble assets

--- Scripts/Food.gd
+++ Scripts/Food.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var gravity
 	pass
 
 
+const GRAVITY = 200.0
+const UNDERWATER = 10.0
+var velocity = Vector2()
 
+func _physics_process(delta):
+	velocity.y += (delta * GRAVITY) / UNDERWATER
 
+	var motion = velocity * delta
+	move_and_collide(motion)
