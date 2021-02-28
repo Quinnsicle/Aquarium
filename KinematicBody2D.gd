b7b1e0d3fc6c7fe32504d6bd53ca60cc104d361d
gunnerganske <gganske15@winona.edu>
2021-02-27 18:43:14 -0600

    Random Generator

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,16 +3,22 @@ extends KinematicBody2D
 var motion = Vector2()
 var state = 0
 #0 for nothing, 1=right, 2=left
-
-func _ready():
-	var scene = load("res://Bubble.tscn")
-	var player = scene.instance()
-	add_child(player)
 	
 func _physics_process(delta):
+	
 	if state == 0:
 		pass
 	elif state == 1:
-		motion.x = 100
+		motion.x = 50
+		motion.y = -10
+	elif state == 2:
+		motion.x = -50
+		motion.y = -10
 
 	move_and_slide(motion, Vector2(0, -1))
+
+
+func _on_Timer_timeout():
+	state = floor(rand_range(0,3))
+	print(state)
+	pass # Replace with function body.
