53058345ea335d658a7698616d059ca356fb5a2e
gunnerganske <gunner.ganske@gmail.com>
2021-02-28 20:48:28 -0600

    Small Bubble Timing Changes

--- Scripts/Bubble.gd
+++ Scripts/Bubble.gd
@@ -5,6 +5,7 @@ var state = 0
 #0 for nothing, 1=right, 2=left
 	
 func _physics_process(delta):
+	motion.y = -20
 	
 	if state == 0:
 		pass
@@ -18,13 +19,13 @@ func _physics_process(delta):
 	move_and_slide(motion, Vector2(0, -1))
 		
 	if is_on_ceiling():
-		
 		var timer = Timer.new()
 		self.add_child(timer)
 		$AnimatedSprite.play("pop")
-		
+		motion.y = 0
+		motion.x = 0
 		timer.connect("timeout", self, "queue_free")
-		timer.set_wait_time(.5)
+		timer.set_wait_time(.1)
 		timer.start()
 		
 
