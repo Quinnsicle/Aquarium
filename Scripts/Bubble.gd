36d09e1cc1d6cfe97fc2f8e74f0a29fc593bc2d3
gunnerganske <gunner.ganske@gmail.com>
2021-02-28 20:36:15 -0600

    Bubble Changes

--- Scripts/Bubble.gd
+++ Scripts/Bubble.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 var motion = Vector2()
 var state = 0
-var velocity = Vector2()
 #0 for nothing, 1=right, 2=left
 	
 func _physics_process(delta):
@@ -17,19 +16,15 @@ func _physics_process(delta):
 		motion.y = -20
 
 	move_and_slide(motion, Vector2(0, -1))
-	
-	#velocity = move_and_slide(velocity)
-	#for i in get_slide_count():
-		#var collision = get_slide_collision(i)
-		#print("Collision", collision.collider.name);
 		
 	if is_on_ceiling():
+		
 		var timer = Timer.new()
 		self.add_child(timer)
 		$AnimatedSprite.play("pop")
 		
 		timer.connect("timeout", self, "queue_free")
-		timer.set_wait_time(.8)
+		timer.set_wait_time(.5)
 		timer.start()
 		
 
