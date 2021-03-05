f93936acc3b1d3d76739b6bcc8d65402ab066645
Quinnsicle <qtoconnor@gmail.com>
2021-03-05 17:16:45 -0600

    Suggestions

--- Scripts/Bubble.gd
+++ Scripts/Bubble.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 var motion = Vector2()
 var state = 0
 #0 for nothing, 1=right, 2=left
-	
+
+### use prefix delta with underscore if delta is never used (_delta)
+### this is causing a warning in the debugger
 func _physics_process(delta):
 	motion.y = -20
 	
@@ -16,6 +18,9 @@ func _physics_process(delta):
 		motion.x = -50
 		motion.y = -20
 
+	### you can set move_and_slide to a variable such as (collision = move_and_slide) 
+	### then you can use that collision to pop the bubble or have the bubble bounce
+	### off the surface such as the wall.
 	move_and_slide(motion, Vector2(0, -1))
 		
 	if is_on_ceiling():
